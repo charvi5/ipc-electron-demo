1490214a5be4d4ec86b46a2b3d24f844ced361c9
Charvi Mittal <mittalcharvi@gmail.com>
2020-05-10 17:01:41 -0700

    Added tests to check IPC and if a button exists by element type

--- tests.js
+++ tests.js
@@ -1,14 +1,26 @@
 // write all tests here
-// import {Selector} from 'testcafe';
-
-// import { testPage } from '../config';
-
+import {Selector} from 'testcafe';
 
 fixture `Electron page`
     .page('./index.html');
 
+// test to check for header
 test('Check page content', async t => {
     var header = await t.eval(() => document.querySelector('body > h1').textContent);
 
     await t.expect(header).eql('TEST ME');
 });
+
+// IPC test to check for button click and communication
+test('Button click to text', async t => {
+    await t.click('#sendSyncMsgBtn')
+    Selector('syncReply').withText('Synchronous message reply: Message Received!');   
+});
+
+// test to check if a button exists using text
+test('Test Button exists', async t => {
+
+    const buttonExists = Selector('button').withText('Sign in with Google').exists;
+    await t.expect(buttonExists).ok();
+});
+
